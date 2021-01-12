b44dffc59caf9012ea034a230dfd75d028e065b8
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:28:18 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/spring/Main.java&&java com/krzem/spring/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/spring/Main.java&&jar cvmf ../manifest.mf ../build/spring.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="spring.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/spring.jar
+:end
