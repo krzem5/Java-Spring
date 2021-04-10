b3fcd3017ea6776ad8da6624de4456e6130fde2d
Krzem <krzem5.dev@gmail.com>
2021-04-10 11:20:50 +0200

    Push Update 04/10/2021, 11:20:33

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/spring/Main.java&&jar cvmf ../manifest.mf ../build/spring.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="spring.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/spring.jar
-:end
+python build.py %*
